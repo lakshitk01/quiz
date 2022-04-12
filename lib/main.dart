0993f1ff4ce795f7e2280835b97c4999bd26a046
lakshit <lakshitk01@gmail.com>
2022-04-12 16:13:47 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Quizer());
+}
+
+class Quizer extends StatelessWidget {
+  const Quizer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey[900],
+        body: const SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Quizpage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Quizpage extends StatefulWidget {
+  const Quizpage({Key? key}) : super(key: key);
+
+  @override
+  _QuizpageState createState() => _QuizpageState();
+}
+
+class _QuizpageState extends State<Quizpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        const Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: Center(
+              child: Text(
+                'this is where the questions go ',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: const Text(
+                'true',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              onPressed: () {
+                //picked true
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.red,
+              child: const Text(
+                'false',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              onPressed: () {
+                //picked false
+              },
+            ),
+          ),
+        ),
+        //add ur questions here
+      ],
+    );
+  }
+}
+
+
+
+
+
+
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
